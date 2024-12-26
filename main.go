be8a5e6b975bd90934e6e93bae3ef5daf2e85f6f
haoran <haoran.mc@outlook.com>
2024-12-26 10:44:09 +0800

    second commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var pathMap map[string]string = make(map[string]string)
+
+func main() {
+	r := gin.Default()
+	r.SetHTMLTemplate(template.Must(template.ParseGlob("layout.html")))
+
+	r.GET("/*path", func(ctx *gin.Context) {
+		p := ctx.Param("path")
+		rawStr := pathMap[p]
+
+		ctx.HTML(http.StatusOK, "layout.html", gin.H{
+			"Data": rawStr,
+		})
+	})
+
+	r.POST("/*path", func(ctx *gin.Context) {
+		p := ctx.Param("path")
+		raw, _ := ctx.GetRawData()
+		pathMap[p] = string(raw)
+	})
+
+	r.Run(":9520")
+}
